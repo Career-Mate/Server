8e65796c5a03e684ede5bf1ce7cd83eb7bde3b97
LEE K.S <kingslee0619@gmail.com>
2025-01-16 18:18:19 +0900

    BaseEntity 추가 및 Auditing 기능 추가

--- src/main/java/UMC/career_mate/CareerMateApplication.java
+++ src/main/java/UMC/career_mate/CareerMateApplication.java
@@ -2,8 +2,10 @@ package UMC.career_mate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CareerMateApplication {
 
 	public static void main(String[] args) {
