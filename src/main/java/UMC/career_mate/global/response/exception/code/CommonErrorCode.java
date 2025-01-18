529d81c1e46fe15887832472d11d0fc204194625
LEE K.S <kingslee0619@gmail.com>
2025-01-18 17:02:10 +0900

    예시 코드 삭제 및 공통 결과 반환 코드, 공통 에러 코드 추가

--- src/main/java/UMC/career_mate/global/response/exception/code/CommonErrorCode.java
+++ src/main/java/UMC/career_mate/global/response/exception/code/CommonErrorCode.java
@@ -7,9 +7,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
-    BAD_REQUEST(400, "EC000", "잘못된 요청입니다."),
-    UNAUTHORIZED(401, "EC001", "인증이 필요합니다."),
-    FORBIDDEN(403, "EC002", "금지된 요청입니다."),
+    BAD_REQUEST(400, "ECM000", "잘못된 요청입니다."),
+    UNAUTHORIZED(401, "ECM001", "인증이 필요합니다."),
+    FORBIDDEN(403, "ECM002", "금지된 요청입니다."),
 
     ;
     private final int status;
