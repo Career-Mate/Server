2152edfe913371e4ddfaf4d7a79b9d8036f1e3a6
KSLEE19 <kingslee0619@gmail.com>
2025-01-21 13:49:14 +0900

    [CMAT-28] FEAT: 회원가입시 빈 플래너 객체 생성

--- src/main/java/UMC/career_mate/domain/member/service/MemberService.java
+++ src/main/java/UMC/career_mate/domain/member/service/MemberService.java
@@ -6,6 +6,8 @@ import UMC.career_mate.domain.member.Member;
 import UMC.career_mate.domain.member.converter.MemberConverter;
 import UMC.career_mate.domain.member.dto.request.JoinMemberDTO;
 import UMC.career_mate.domain.member.repository.MemberRepository;
+import UMC.career_mate.domain.planner.dto.request.CreatePlannerDTO;
+import UMC.career_mate.domain.planner.service.PlannerCommandService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,11 +19,18 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final JobService jobService;
+    private final PlannerCommandService plannerService;
 
     @Transactional
     public Member joinMember(JoinMemberDTO request) {
         Job job = jobService.findJobById(request.job());
+
+        //새로운 회원 객체 생성
         Member newMember = MemberConverter.toEntity(request, job);
+
+        //비어있는 플래너 생성
+        CreatePlannerDTO createPlannerDTO = CreatePlannerDTO.builder().build();
+        plannerService.savePlanner(newMember,createPlannerDTO);
 
         return memberRepository.save(newMember);
     }
