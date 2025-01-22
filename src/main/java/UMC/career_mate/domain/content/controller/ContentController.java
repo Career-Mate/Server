dbd14bd6c6ba2a1eeb2db87ab3be5872a94ba440
minkyeong <haileyre000b@naver.com>
2025-01-23 03:13:22 +0900

    [CMAT-21] feat: 직무별 컨텐츠 목록 조회 api 구현

--- src/main/java/UMC/career_mate/domain/content/controller/ContentController.java
+++ src/main/java/UMC/career_mate/domain/content/controller/ContentController.java
@@ -3,6 +3,7 @@ package UMC.career_mate.domain.content.controller;
 import UMC.career_mate.domain.content.dto.request.ContentRequestDTO;
 import UMC.career_mate.domain.content.dto.response.ContentResponseDTO;
 import UMC.career_mate.domain.content.service.ContentService;
+import UMC.career_mate.domain.job.Service.JobService;
 import UMC.career_mate.domain.member.Member;
 import UMC.career_mate.global.annotation.LoginMember;
 import UMC.career_mate.global.response.ApiResponse;
@@ -22,13 +23,14 @@ import java.util.List;
 @RequestMapping("/content")
 public class ContentController {
 
+    private final JobService jobService;
     private final ContentService contentService;
 
     @PostMapping // 컨텐츠 업로드 (관리자용)
     @Operation(
             summary = "컨텐츠 업로드 API",
             description = """
-            관리자가 직무에 맞는 컨텐츠를 업로드할 때 사용합니다.
+            관리자가 api로 컨텐츠를 업로드할 때 사용합니다.
             요청 바디에 아래 필드를 포함해야 합니다.
             - `title`: 컨텐츠 제목 (필수)
             - `url`: 컨텐츠 링크 (필수)
@@ -40,25 +42,25 @@ public class ContentController {
         return ApiResponse.onSuccess(contentService.uploadContent(contentRequestDTO));
     }
 
-    @GetMapping // 로그인한 사용자의 직무 기반 콘텐츠 조회
+    @GetMapping // 직무별 콘텐츠 조회
     @Operation(
-            summary = "로그인 사용자의 직무별 콘텐츠 조회 API",
+            summary = "직무별 컨텐츠 조회 API",
             description = """
-            로그인된 사용자의 회원 정보에 있는 직무 ID를 바탕으로 관련 콘텐츠를 조회합니다.
-            페이지네이션 지원. 인증된 사용자만 접근 가능.
-            """
+        요청된 직무 ID에 따라 해당 직무의 콘텐츠를 조회합니다.
+        Query Parameters를 통해 jobid를 받고, 만약 해당 jobid가 db에 존재하지 않는다면 EJB000, 400 에러코드를 띄웁니다.
+        요청 파라미터:
+        - `jobId`: 조회할 직무 ID (필수)
+        - `page`: 페이지 번호 (기본값: 1)
+        - `size`: 페이지 크기 (기본값: 10)
+        """
     )
-    public ApiResponse<List<ContentResponseDTO>> getContentsByUserJob(
-            @LoginMember Member member,
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size) {
+    public ApiResponse<?> getContentsByJobId(@RequestParam Long jobId,
+                                             @RequestParam(defaultValue = "1") int page,
+                                             @RequestParam(defaultValue = "10") int size) {
+        // 직무 ID 검증
+        jobService.findJobById(jobId);
 
-        // 로그인한 사용자의 직무 ID를 가져옵니다.
-        Long jobId = member.getJob().getId();
-
-        // 서비스 계층에서 콘텐츠 조회
+        // 콘텐츠 조회
         return ApiResponse.onSuccess(contentService.getContentsByJobId(jobId, page, size));
     }
-
-
 }
