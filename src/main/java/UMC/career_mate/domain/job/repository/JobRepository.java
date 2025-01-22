66b342d196ead8c66b03886c701290da428dd689
KSLEE19 <kingslee0619@gmail.com>
2025-01-22 18:53:39 +0900

    [CMAT-29] FEAT : 모든 직무 조회 API 구현

--- src/main/java/UMC/career_mate/domain/job/repository/JobRepository.java
+++ src/main/java/UMC/career_mate/domain/job/repository/JobRepository.java
@@ -3,5 +3,9 @@ package UMC.career_mate.domain.job.repository;
 import UMC.career_mate.domain.job.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+    List<Job> findAll();
 }
