62c098519682a4f962a0504326e0400d31a8206c
minkyeong <haileyre000b@naver.com>
2025-01-23 11:52:17 +0900

    [CMAT-21] fix: converter 분리, 빈페이지 처리, transactional 추가

---
+++ src/main/java/UMC/career_mate/domain/content/converter/ContentConverter.java
@@ -0,0 +1,30 @@
+package UMC.career_mate.domain.content.converter;
+
+import UMC.career_mate.domain.content.Content;
+import UMC.career_mate.domain.content.dto.request.ContentRequestDTO;
+import UMC.career_mate.domain.content.dto.response.ContentResponseDTO;
+import UMC.career_mate.domain.job.Job;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContentConverter {
+
+    public Content toContent(ContentRequestDTO contentRequestDTO, Job job) {
+        return Content.builder()
+                .title(contentRequestDTO.title())
+                .url(contentRequestDTO.url())
+                .photo(contentRequestDTO.photo())
+                .job(job)
+                .build();
+    }
+
+    public ContentResponseDTO toContentResponseDTO(Content content) {
+        return ContentResponseDTO.builder()
+                .id(content.getId())
+                .title(content.getTitle())
+                .url(content.getUrl())
+                .photo(content.getPhoto())
+                .jobId(content.getJob().getId())
+                .build();
+    }
+}
