de13f8d7be4e4297790c46e2055ea80f451faf99
KSLEE19 <kingslee0619@gmail.com>
2025-01-24 20:32:38 +0900

    [CMAT-12] FEAT : 예외처리 추가

--- src/main/java/UMC/career_mate/global/security/service/SecurityService.java
+++ src/main/java/UMC/career_mate/global/security/service/SecurityService.java
@@ -1,6 +1,8 @@
 package UMC.career_mate.global.security.service;
 
 import UMC.career_mate.domain.member.enums.SocialType;
+import UMC.career_mate.global.response.exception.GeneralException;
+import UMC.career_mate.global.response.exception.code.CommonErrorCode;
 import UMC.career_mate.global.security.jwt.repository.RefreshTokenRepository;
 import UMC.career_mate.global.security.util.CookieUtil;
 import UMC.career_mate.global.security.util.JwtUtil;
@@ -20,12 +22,13 @@ public class SecurityService {
     @Transactional
     public void logout(HttpServletRequest request, HttpServletResponse response) {
         String refreshToken = CookieUtil.getCookieValue(request, response, "refresh-token");
-        SocialType socialType = jwtUtil.getSocialType(refreshToken);
 
         if (refreshToken != null) {
             //redis에서 리프레시 토큰 삭제
             Long memberId = jwtUtil.getMemberId(refreshToken);
             repository.deleteById(memberId);
+        } else {
+            throw new GeneralException(CommonErrorCode.INVALID_TOKEN);
         }
 
         //쿠키에서 토큰 삭제
