17a3359117e9f457116691a92fbaf8e70fac8592
minkyeong <haileyre000b@naver.com>
2025-01-25 12:44:55 +0900

    [CMAT-38] feat: 직무별 추천 컨텐츠 조회 api에 스크랩 여부 추가

--- src/main/java/UMC/career_mate/domain/content/service/ContentService.java
+++ src/main/java/UMC/career_mate/domain/content/service/ContentService.java
@@ -6,8 +6,10 @@ import UMC.career_mate.domain.content.converter.ContentConverter;
 import UMC.career_mate.domain.content.dto.request.ContentRequestDTO;
 import UMC.career_mate.domain.content.dto.response.ContentResponseDTO;
 import UMC.career_mate.domain.content.repository.ContentRepository;
+import UMC.career_mate.domain.contentScrap.repository.ContentScrapRepository;
 import UMC.career_mate.domain.job.Job;
 import UMC.career_mate.domain.job.Service.JobService;
+import UMC.career_mate.domain.member.Member;
 import UMC.career_mate.global.common.PageResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,6 +26,7 @@ public class ContentService {
 
     private final ContentRepository contentRepository;
     private final JobService jobService;
+    private final ContentScrapRepository contentScrapRepository;
 
     public ContentResponseDTO uploadContent(ContentRequestDTO contentRequestDTO) {
         // Job ID 유효성 확인
@@ -37,16 +40,22 @@ public class ContentService {
     }
 
     @Transactional(readOnly = true)
-    public PageResponseDTO<List<ContentResponseDTO>> getContentsByJobId(Long jobId, int page, int size) {
-        // Job 유효성 확인
+    public PageResponseDTO<List<ContentResponseDTO>> getContentsByJobId(Long jobId, int page, int size, Member member) {
+        // Job ID 유효성 확인
         jobService.findJobById(jobId);
 
         PageRequest pageRequest = PageRequest.of(page - 1, size);
         Page<Content> contentPage = contentRepository.findByJobId(jobId, pageRequest);
 
-        // 빈페이지를 따로 처리하지 않고 PageResponseDTO로 감싸서 반환하는 방식으로 바꾸었습니다!
+        // 사용자가 스크랩한 콘텐츠들 id 가져오기
+        List<Long> scrappedContentIds = contentScrapRepository.findByMember(member)
+                .stream()
+                .map(scrap -> scrap.getContent().getId())
+                .toList();
+
+        // 컨텐츠가 위 목록에 있는지 확인하며 isScrapped 값을 포함(true) 포함x(false) 설정
         List<ContentResponseDTO> contentList = contentPage.stream()
-                .map(ContentConverter::toContentResponseDTO)
+                .map(content -> ContentConverter.toContentResponseDTOWithScrapStatus(content, scrappedContentIds.contains(content.getId())))
                 .toList();
 
         boolean hasNext = contentPage.hasNext();
