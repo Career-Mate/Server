779d1b37332372a77f33b361de10a467dd80ef41
veronees <jsg6854@naver.com>
2025-01-29 00:58:03 +0900

    [CMAT-42] feat: 채용 공고 조회 시 사용자의 인턴, 프로젝트 템플릿 답변 데이터를 기반으로 gpt에게 요청하도록 로직을 구현한다

---
+++ src/main/java/UMC/career_mate/domain/chatgpt/GptAnswer.java
@@ -0,0 +1,52 @@
+package UMC.career_mate.domain.chatgpt;
+
+import UMC.career_mate.domain.member.Member;
+import UMC.career_mate.domain.recruit.enums.RecruitKeyword;
+import UMC.career_mate.global.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "gpt_answers")
+@SQLRestriction("deleted_at is NULL")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class GptAnswer extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "gpt_answer_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private RecruitKeyword recruitKeyword;
+
+    private int careerYear;
+
+    public void updateData(RecruitKeyword recruitKeyword, int careerYear) {
+        this.recruitKeyword = recruitKeyword;
+        this.careerYear = careerYear;
+        super.setUpdatedAt();
+    }
+}
