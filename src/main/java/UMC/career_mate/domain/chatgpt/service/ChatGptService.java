823dcbfad35689d70c67482119af3b2dbed3c031
veronees <jsg6854@naver.com>
2025-02-09 21:54:59 +0900

    [CMAT-57] feat: 프론트, 백에 따라 gpt 질문 포맷을 분리한다

--- src/main/java/UMC/career_mate/domain/chatgpt/service/ChatGptService.java
+++ src/main/java/UMC/career_mate/domain/chatgpt/service/ChatGptService.java
@@ -1,6 +1,7 @@
 package UMC.career_mate.domain.chatgpt.service;
 
 import UMC.career_mate.domain.chatgpt.dto.api.response.ChatCompletionResponse;
+import UMC.career_mate.domain.job.Job;
 import UMC.career_mate.domain.recruit.dto.MemberTemplateAnswerDTO;
 import UMC.career_mate.domain.chatgpt.dto.api.request.GptRequest;
 import UMC.career_mate.domain.chatgpt.dto.api.request.GptRequest.Message;
@@ -10,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,10 +36,16 @@ public class ChatGptService {
         "이 사람의 경력을 계산해서 앞뒤 설명 하지말고 정수로만 올바른 답변 예시와 같은 형식으로 답변해줘. " +
             "올바론 답변 예시) 5, 잘못된 답변 예시 1) 5년, 잘못된 답변 예시 2) 5 years, 잘못된 답변 예시 3) 이 사람의 경력은 5년";
 
-    private static final String GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD =
+    private static final String GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD_BACKEND =
         "이 사람의 직무를 내가 제시한 보기들 중에서 하나만 골라서 답변해줘.\n" +
-            "직무 보기 :'BACKEND', 'BACKEND_SPRING', 'BACKEND_NODE', 'BACKEND_DJANGO', 'FRONTEND', 'DESIGNER', 'PM', " +
-            "이 중에서 적절한 직무가 없다면 'MISMATCH'로 답변해줘.\n" +
+            "직무 보기 :'BACKEND_SPRING', 'BACKEND_NODE', 'BACKEND_DJANGO'. " +
+            "이 중에서 적절한 직무를 고르기 애매하면 'BACKEND'로 답변해줘.\n" +
+            "앞뒤 설명하지 말고 직무만 답변해줘.";
+
+    private static final String GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD_FRONTEND =
+        "이 사람의 직무를 내가 제시한 보기들 중에서 하나만 골라서 답변해줘.\n" +
+            "직무 보기 : 'FRONTEND_REACT', 'FRONTEND_IOS', 'FRONTEND_ANDROID'. " +
+            "이 중에서 적절한 직무를 고르기 애매하면 'FRONTEND'로 답변해줘.\n" +
             "앞뒤 설명하지 말고 직무만 답변해줘.";
 
     private static final String GPT_REQUEST_FORMAT_FOR_COMMENT =
@@ -45,6 +53,11 @@ public class ChatGptService {
             "친근한 말투로 어떤 포지션이 어울리는지, 어떤 경험을 어필하면 좋을지와 같은 내용으로 조언 문구를 생성해서 답변해줘. " +
             "답변은 문구만 답변해줘. 문구를 생성할 때 내용에는 회사 이름은 제외해줘. " +
             "답변에서 '-'는 빼줘.";
+
+    private static final Map<String, String> formatMap = Map.ofEntries(
+        Map.entry("백엔드 개발자", GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD_BACKEND),
+        Map.entry("프론트엔드 개발자", GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD_FRONTEND)
+    );
 
     private static final String GPT_SYSTEM_ROLE = "너는 취업 전문가로서 내가 보낸 경험 데이터를 기반으로 '~~한 경험이 있는 000님, ~~한 경험을 어필해보면 어때요?' 라는 느낌으로 사용자 맞춤형 추천 문구를 작성한다. " +
         "문구의 말투는 '-니다'체를 사용하는 것이 아니라, '-요'체를 사용한다.";
@@ -61,9 +74,9 @@ public class ChatGptService {
         return Integer.parseInt(gptAnswer);
     }
 
-    public RecruitKeyword getRecruitKeyword(String chatGptRequestContent) {
+    public RecruitKeyword getRecruitKeyword(String chatGptRequestContent, Job job) {
         GptRequest gptRequest = createGptRequest(
-            chatGptRequestContent + GPT_REQUEST_FORMAT_POSTFIX_FOR_RECRUIT_KEYWORD);
+            chatGptRequestContent + formatMap.get(job.getName()));
 
         ObjectMapper om = new ObjectMapper();
         String gptAnswer = getGptAnswer(om, gptRequest);
