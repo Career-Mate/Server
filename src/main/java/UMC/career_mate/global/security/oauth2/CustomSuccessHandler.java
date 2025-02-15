72d917835adbaffed9297119aab0d6f51e4b0d71
LEE K.S <kingslee0619@gmail.com>
2025-02-15 17:26:35 +0900

    fix : 로그인 성공 시 이름을 쿼리파라미터로 반환하도록 변경

--- src/main/java/UMC/career_mate/global/security/oauth2/CustomSuccessHandler.java
+++ src/main/java/UMC/career_mate/global/security/oauth2/CustomSuccessHandler.java
@@ -45,8 +45,8 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         if (!member.getIs_complete()) {
             //추가 정보가 입력되어 있지 않을 경우
-            response.sendRedirect(FRONTEND_BASE_URL + FRONTEND_PROFILE_PATH);
-
+            String name = member.getName();
+            response.sendRedirect(FRONTEND_BASE_URL + FRONTEND_PROFILE_PATH + "?name=" + name);
         } else {
             // 추가 정보가 입력된 경우
             response.sendRedirect(FRONTEND_BASE_URL);
